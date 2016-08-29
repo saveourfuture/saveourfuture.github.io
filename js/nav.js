5188bd8f97bca95100f67900fa7b38d4cd8becec
Piotr Kacprzak <marzena.kacprzak@digitalskillsacademy.me>
2016-08-29 01:20:45 +0100

    news + get in touch + some bug fixes

---
+++ js/nav.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function() {
+
+    var a = $(".navbar-background").offset().top;
+
+    $(document).scroll(function () {
+        if ($(this).scrollTop() > a) {
+            $('.navbar-background').css({"background": "#f2f2f2"});
+        } else {
+            $('.navbar-background').css({"background": "transparent"});
+        }
+    });
+
+    $("#button-scroll").click(function () {
+        $('html, body').animate({
+            scrollTop: $("#scroll-target").offset().top
+        }, 2000);
+    });
+
+
+});
+
+
