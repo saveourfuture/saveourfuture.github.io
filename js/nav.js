b367a3895f6478887e135ba24ea3746858079ad5
Piotr Kacprzak <marzena.kacprzak@digitalskillsacademy.me>
2016-08-29 23:57:33 +0100

    comments

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,11 @@
+<!--
+* Author:	Marzena Kacprzak
+*	Assignment:	Minor	Project	– Save Our Future
+*	Student	ID:
+*	Date	:	2016/08/29
+*	Ref:	getbootstrap.com
+-->
+
 
 $(document).ready(function() {
 
