1357b0880322cd5d8d5a08f9ba32a60525aeb3fd
Piotr Kacprzak <marzena.kacprzak@digitalskillsacademy.me>
2016-09-10 00:08:27 +0100

    calculator

--- js/quiz.js
+++ js/quiz.js
@@ -1,32 +1,68 @@
 
 
-$(function(){
-    var loading = $('#loadbar').hide();
-    $(document)
-        .ajaxStart(function () {
-            loading.show();
-        }).ajaxStop(function () {
-        loading.hide();
+$(document).ready(function(){
+    $("#flip1").click(function(){
+        $("#panel1").slideDown("slow");
     });
+});
 
-    $("label.btn").on('click',function () {
-        var choice = $(this).find('input:radio').val();
-        $('#loadbar').show();
-        $('#quiz').fadeOut();
-        setTimeout(function(){
-            $( "#answer" ).html(  $(this).checking(choice) );
-            $('#quiz').show();
-            $('#loadbar').fadeOut();
-            /* something else */
-        }, 1500);
+
+
+$(document).ready(function(){
+    $("#flip2").click(function(){
+        $("#panel2").slideDown("slow");
     });
+});
 
-    $ans = 3;
 
-    $.fn.checking = function(ck) {
-        if (ck != $ans)
-            return 'INCORRECT';
-        else
-            return 'CORRECT';
-    };
+
+$(document).ready(function(){
+    $("#flip3").click(function(){
+        $("#panel3").slideDown("slow");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#flip4").click(function(){
+        $("#panel4").slideDown("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#flip5").click(function(){
+        $("#panel5").slideDown("slow");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#flip6").click(function(){
+        $("#panel6").slideDown("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#flip7").click(function(){
+        $("#panel7").slideDown("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#flip8").click(function(){
+        $("#panel8").slideDown("slow");
+    });
+});
+
+
+$(document).ready(function(){
+
+    $("#submit-quiz").click(function(){
+        $("#quiz-result").show();
+    });
 });
