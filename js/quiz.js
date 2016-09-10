88b9150d4bbc9c2b6c3f610f1b48d562bd451714
saveourfuture <marzena.kacprzak@digitalskillsacademy.me>
2016-09-10 01:55:09 +0100

    Calculator (#5)
    * plus join up
    
    * calculator
    
    * calculator
    
    * calculator
    
    * calculator
    
    * calculator

---
+++ js/quiz.js
@@ -0,0 +1,69 @@
+
+
+$(document).ready(function(){
+    $("#flip1").click(function(){
+        $("#panel1").slideDown("slow");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#flip2").click(function(){
+        $("#panel2").slideDown("slow");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#flip3").click(function(){
+        $("#panel3").slideDown("slow");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#flip4").click(function(){
+        $("#panel4").slideDown("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#flip5").click(function(){
+        $("#panel5").slideDown("slow");
+    });
+});
+
+
+
+$(document).ready(function(){
+    $("#flip6").click(function(){
+        $("#panel6").slideDown("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#flip7").click(function(){
+        $("#panel7").slideDown("slow");
+    });
+});
+
+
+$(document).ready(function(){
+    $("#flip8").click(function(){
+        $("#panel8").slideDown("slow");
+    });
+});
+
+
+$(document).ready(function(){
+
+    $("#submit-quiz").click(function(){
+        $("#quiz-result").show();
+    });
+});
+
